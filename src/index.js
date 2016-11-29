ddb7e149c0d98a86fb17018cab25311bbaf04b2e
Wojciech Jasinski <wojtek@proxoft.se>
2016-11-29 09:33:24 +0100

    Created Redux Actions, Reducers and Store and hooked everything up

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
 import {Router, browserHistory} from 'react-router';
+import {Provider} from 'react-router';
+import configureStore from './store/configureStore';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-  <Router history={browserHistory} routes={routes}/>,
-  document.getElementById('app')
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes}/>,
+    document.getElementById('app')
+  </Provider>
 );
-
-
